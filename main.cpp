54082db547b07a16ac4ef6d9124439b60536b383
Кадников Николай <25roter52@gmail.com>
2022-10-30 15:23:29 +0400

    fix: constructors, adding a number, deleting, operators +, +=, -, -=, add: printing the elements of an objects, finding an intersection of objects

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <stdio.h>
-#include <typeinfo>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include <cmath>
 #include <iostream>
+using namespace std;
 
-class Set_of_numbers
+
+
+
+class set_of_numbers
 {
     private:
 
@@ -12,180 +19,320 @@ class Set_of_numbers
 
     public:
 
-    Set_of_numbers( int size = 0 )
+
+    const int presence ( const int & number )
     {
-        this->size = size;
+
+
+        for( int i = 0; i < this->count; i++ )
+        {
+            if ( this->array[i] == number ) return i;
+        }
+        return -1;
+
+
+    }// Проверка наличия
+
+
+
+
+    const int & operator [] ( const int & index )
+    {
+
+
+        if ( index > count - 1 || index < 0 ) throw "Неверный индекс\n";
+        return array[index];
+
+
+    }// Обращение по индексу
+
+
+
+    set_of_numbers( int size = 0 )
+    {
+        size = size;
         count = 0;
         array = new int [size];
         for ( int i = 0; i < size; i++ )
         {
-            int choice;
-            printf( "Закончить создание - 1\nПродолжить создание - 2\n" );
-            scanf( "%d", & choice );
-            if ( choice == 1 )
+            while( 1 )
             {
-                break;
-            }
-            double number;
-            bool flag = 1;
-            while ( flag )
-            {
-                printf( "Введите целое число: \n" );
-                scanf( "%lf", & number );
-                if ( double(int(number)) != number ) printf( "Введено нецелое число\n" );
-                else 
-                {
-                    bool unic_number = 1;
-                    for ( int j = 0; j <= i; j++ )
-                    {
-                        if ( array[j] == number )
-                        {
-                            printf( "Число %d не уникальное\n", int(number) );
-                            unic_number = 0;
-                            break;
-                        }
-                        else continue;                       
-                    }
-                    if ( unic_number == 1 )
-                    {
-                        array[i] = int(number);
-                        count++;
-                        flag = 0;
-                    }
+
+
+                int number;
+                cout << "Введите целое число: ";
+                cin >> number;                
+                if ( this->presence( number ) )
+                {                 
+                    cout << "Число" << number << "не уникальное" << endl;
+                    continue;
                 }
+                else
+                {
+                    array[i] = number;
+                    count++;
+                    break;
+                }     
+
+
             }
         }
     }// Конструктор
 
-    Set_of_numbers( Set_of_numbers & set, int size ) 
+
+
+
+    set_of_numbers( const set_of_numbers & other_set ) 
     {
-        this->size = size;
-        for ( int i = 0; i < size; i++ )
+
+
+        this->size = other_set.size;
+        this->count = other_set.count;
+        this->array = new int [this->size];
+        for ( int i = 0; i < other_set.count; i++ )
         {
-            array[i] = set.array[i];
+            this->array[i] = other_set.array[i];
         }
+
+
     }// Конструктор копирования
 
-    bool Presence ( int number )
-    {
-        for( int i = 0; i < this->count; i++ )
-        {
-            if ( this->array[i] == number ) return true;
-        }
-        return false;
-    }// Проверка наличия
 
-    int & operator []( int index )
-    {
-        if ( index > size || index < 1 ) throw "Неверный индекс\n";
-        return array[index];
-    }// Обращение по индексу
 
-    void Add ( int number )
+
+    void add ( const int & number )
     {
-        if ( this->count == this->size )
+
+        if ( this->presence( number ) != -1 ) throw "Введенное число не уникальное\n";
+        if ( this->count == this->size ) 
         {
-            int * old_arr = new int [this->count];
+            int * old_arr = new int [this->size];
             for ( int k = 0; k < this->count; k++ )
             {
                 old_arr[k] = this->array[k];
             }
             this->array = new int [this->size + 10];
+            for ( int k = 0; k < this->count; k++ )
+            {
+                this->array[k] = old_arr[k];
+            }
+            this->size += 10;
+            delete[] old_arr;
+            this->array[count++] = number;
+        }
+        else  this->array[count++] = number;
+
+    }// Добавление числа в массив
+
+
+
+    void delete_ ( const int & number )
+    {
+
+        int value = this->presence( number );
+        if( value != -1 )
+        {
+
+            int * old_arr = new int [this->size];
             for ( int k = 0; k < this->size; k++ )
+            {
+                old_arr[k] = this->array[k];
+            }
+            this->array = new int [--this->size];
+            for ( int k = 0; k < value; k++ )
+            {
+                this->array[k] = old_arr[k];
+            }
+            for ( int k = value + 1; k < this->size; k++ )
             {
                 this->array[k] = old_arr[k];
             }
             delete[] old_arr;
+            this->count--;
         }
-        this->array[++count] = number;
-    }// Добавление числа в массив
+        else throw "Такого числа нет\n";
 
-    void Delete_ ( int number )
-    {
-        for ( int i = 0; i < count; i++ )
-        {   
-            if ( array[i] == number )
-            {
-                int * old_arr = new int [this->count];
-                this->array = new int [this->size - 1];
-                for ( int k = 0; k < i; k++ )
-                {
-                    this->array[k] = old_arr[k];
-                }
-                for ( int k = i + 1; k < count - 1; k++ )
-                {
-                    this->array[k] = old_arr[k];
-                }
-                delete[] old_arr;                        
-                this->count--;
-            }
-        }
     }// Удаление числа из массива
 
-    Set_of_numbers & operator + ( Set_of_numbers & set_1 )
+
+    void print()
     {
-        if ( set_1.count == 0 ) return *this;
-        for ( int j = 0; j < set_1.count; j++ )
+        for( int i = 0; i < count; i++ ) 
         {
-            if ( !( this->Presence( set_1.array[j] ) ) )
+            cout << array[i] << endl;
+        }
+    }
+
+
+
+    set_of_numbers & operator + ( const set_of_numbers & set_1 )
+    {
+
+        set_of_numbers new_set( 0 );
+        new_set.size = this->count + set_1.count;
+        new_set.array = new int [new_set.size];
+        for( int i = 0; i < this->count; i++ )
+        {
+            new_set.array[i] = this->array[i];
+            new_set.count++;
+        }
+        for ( int i = 0; i < set_1.count; i++ )
+        {
+            if ( ( this->presence( set_1.array[i] ) ) == -1 )
             {
-                *this += set_1.array[j]
+                new_set.array[++new_set.count] = set_1.array[i];
+                new_set.count++;
             }
         }
-        return *this;
+        return new_set;
+
+
     }// Сложение двух массивов
 
-    Set_of_numbers & operator - ( Set_of_numbers & set_1 )
+
+
+    set_of_numbers & operator - ( const set_of_numbers & set_1 )
     {
-        if ( set_1.size == 0 ) return *this;
-        for ( int i = 0; i < set_1.size; i++ )
+        set_of_numbers new_set = *this;
+        for ( int i = 0; i < set_1.count; i++ )
         {
-            this->Delete_( set_1.array[i] );
+            try
+            {
+
+            new_set.delete_( set_1.array[i] );
+
+            }
+            catch( const char* e )
+            {
+
+            continue;
+
+            }
+            catch( ... )
+            {
+
+                throw "Something went wrong\n";
+
+            }
         }
-        }
-        this->size = this->count;
-        return *this;
+        return new_set;
     }// Вычитание двух массивов
 
-    void operator + ( int number )
+
+
+    void operator + ( const int& number )
     {
+
         
-        if( !( this->Presence( number ) ) )
+        try
         {
-            this->Add( number )
+            this->add( number );
+        }
+        catch( const char* e )
+        {
+            throw "Такое число уже есть\n";
+        }
+        catch( ... )
+        {
+
+            throw "Something went wrong\n";
+
+        }
+
+
+
+    }// Прибавить число в массив
+
+
+
+    void operator += ( const int& number )
+    {
+        try
+        {
+            this->add( number );
+        }
+        catch( const char* e )
+        {
+            throw "Такое число уже есть\n";
+        }
+        catch( ... )
+        {
+            throw "Something went wrong\n";
         }
     }// Прибавить число в массив
 
-    void operator += ( int number )
-    {
-          
-        if( !( this->Presence( number ) ) )
-        {
-            this->Add( number )
-        }
-    }// Прибавить число в массив
 
-    void operator - ( int number )
+
+    void operator - ( const int& number )
     {
-        if( this->Presence( number ) )
+
+
+        try
         {
-        this->Delete_( number )
+        this->delete_( number );
+        }
+        catch( const char* e )
+        {
+            throw e;
+        }
+        catch( ... )
+        {
+            throw "Something went wrong";
+        }
+
+
+
+    }// Удалить число из массива
+
+
+
+    void operator -= ( const int & number )
+    {
+        try
+        {
+        this->delete_( number );
+        }
+        catch( const char* e )
+        {
+            throw e;
+        }
+        catch( ... )
+        {
+            throw "Something went wrong";
         }
     }// Удалить число из массива
 
-    void operator -= ( int number )
-    {
-        if( this->Presence( number ) )
-        {
-        this->Delete_( number )
-        }
-    }// Удалить число из массива
 
-    Set_of_numbers & intersection ( Set_of_numbers & arr2 )
+
+    set_of_numbers & intersection( const set_of_numbers & set_2 )
+    {
+
+        set_of_numbers new_arr ( 0 );
+        for ( int i = 0; i < this->count; i++ )
+        {
+
+
+            if( set_2->presense( this->array[i] ) ) new_arr += this->array[i];
+
+
+        }
+
+        return new_arr;
+    }// Пересечение
+
+    bool occurrence_of_elements(const set_of_numbers& arr2)
+    {
+    
+    }
+
+
 };
 
 int main()
 {
+    setlocale( LC_ALL, "Russian" );
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     printf( "Start\n Введите размерность масива: \n" );
     int size_1;
     scanf( "%d", &size_1 );
